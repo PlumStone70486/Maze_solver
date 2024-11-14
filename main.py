baab3156d934cf1c3412957860cf0779a3c92832
PlumStone70486 <tsbg95@gmail.com>
2024-11-14 14:47:52 +0100

    Added Point and Line class, added draw_line

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from window import *
 def main():
 
     win = Window(800, 600)
+    point1 = Point(765, 18)
+    point2 = Point(28, 588)
+    line = Line(point1, point2)
+    win.draw_line(line, "black")
     win.wait_for_close()
 
 main()
