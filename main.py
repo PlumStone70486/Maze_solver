b17b2663e533a4781eefdf2820bcd389df62b15e
PlumStone70486 <tsbg95@gmail.com>
2024-11-16 08:50:10 +0100

    Added cell.py

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from window import *
+from cell import *
 
 def main():
 
     win = Window(800, 600)
-    point1 = Point(765, 18)
-    point2 = Point(28, 588)
-    line = Line(point1, point2)
-    win.draw_line(line, "black")
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50, 50, 100, 100)
+
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
     win.wait_for_close()
 
 main()
