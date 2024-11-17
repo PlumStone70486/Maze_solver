2ff4cd195d95b51df99e1ac7db85e556d97a5d4d
PlumStone70486 <tsbg95@gmail.com>
2024-11-17 14:01:05 +0100

    Added break walls function in maze.py

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ from maze import *
 
 def main():
     
-    num_rows = 4
-    num_cols = 5
+    num_rows = 3
+    num_cols = 4
     margin = 10
     screen_x = 800
     screen_y = 600
     cell_size_x = (screen_x - 2 * margin) / num_cols
     cell_size_y = (screen_y - 2 * margin) / num_rows
     win = Window(screen_x, screen_y)
-    maze = Maze(margin, margin, num_cols, num_rows, cell_size_x, cell_size_y, win)
+    maze = Maze(margin, margin, num_cols, num_rows, cell_size_x, cell_size_y, win, 10)
     """
     win = Window(800, 600)
     cell = Cell(win)
--- maze.py
+++ maze.py
@@ -1,9 +1,10 @@
 import time
+import random
 from window import *
 from cell import *
 
 class Maze:
-    def __init__(self, x1, y1, num_cols, num_rows, cell_size_x, cell_size_y, win=None):
+    def __init__(self, x1, y1, num_cols, num_rows, cell_size_x, cell_size_y, win=None, seed=None):
         self._cells = []
         self._x1 = x1
         self._y1 = y1
@@ -12,8 +13,14 @@ class Maze:
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
         self._win = win
+        if seed is not None:
+            random.seed(seed)
         self._create_cells()
         self._break_entrance_and_exit()
+        self._break_walls_r(0, 0)
+        
+            
+        
 
     def _create_cells(self):
         for i in range(self._num_cols):
@@ -48,4 +55,55 @@ class Maze:
         self._draw_cell(0, 0)
         self._cells[-1][-1].has_bottom_wall = False
         self._draw_cell(len(self._cells) - 1, len(self._cells[0]) - 1)
+
+    def _is_in_bounds(self, i, j):
+        return 0 <= i < len(self._cells) and 0 <= j < len(self._cells[0])
+
+    def _break_walls_r(self, i, j):
+        current_cell = self._cells[i][j]
+        current_cell.visited = True
+        while True:
+            to_visit = []
+            if self._is_in_bounds(i + 1, j):
+                if self._cells[i+1][j].visited is False:
+                    to_visit.append((i + 1, j))
+
+            if self._is_in_bounds(i, j + 1):
+                if self._cells[i][j+1].visited is False:
+                    to_visit.append((i, j + 1))
+
+            if self._is_in_bounds(i - 1, j):
+                if self._cells[i-1][j].visited is False:
+                    to_visit.append((i - 1, j))
+
+            if self._is_in_bounds(i, j - 1):
+                if self._cells[i][j-1].visited is False:
+                    to_visit.append((i, j - 1))
+
+            if len(to_visit) == 0:
+                return
+            else:
+                chosen_i , chosen_j = random.choice(to_visit)
+                chosen_cell = self._cells[chosen_i][chosen_j]
+
+                if chosen_i == i + 1 and chosen_j == j:
+                    current_cell.has_bottom_wall = False
+                    chosen_cell.has_top_wall = False
+
+                if chosen_i == i and chosen_j == j + 1:
+                    current_cell.has_right_wall = False
+                    chosen_cell.has_left_wall = False
+
+                if chosen_i == i - 1 and chosen_j == j:
+                    current_cell.has_top_wall = False
+                    chosen_cell.has_bottom_wall = False
+
+                if chosen_i == i and chosen_j == j - 1:
+                    current_cell.has_left_wall = False
+                    chosen_cell.has_right_wall = False
+                    
+                self._draw_cell(i, j)
+                self._draw_cell(chosen_i, chosen_j)
+                self._break_walls_r(chosen_i, chosen_j)
+
 
